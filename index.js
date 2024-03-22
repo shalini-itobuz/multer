52131f0c80e6ea497046f7cd27859c97f23ec7c3
shalini-itobuz <shalini@itobuz.com>
2024-03-22 12:48:04 +0530

    updated

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-const express= require('express')
-const multer  = require('multer')
-const app=express()
+import express from 'express';
+import multer from 'multer';
+
+const app = express();
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads/')
+    destination: function(req , file , cb){
+        cb(null , 'uploads/')
     },
-    filename: function (req, file, cb) {
-        // console.log("inside storage",file)
-      cb(null,Math.floor(Math.random()*100000)+(file.originalname))
-    }
-  })
-  const upload = multer({ storage: storage })
-// require("dotenv").config()
-app.post('/uploading',upload.single('file1'),(req,res)=>{
-    console.log(req.file)
+    filename: function (req , file ,cb){
+        cb(null,`${Date.now()}${file.originalname}`)
+    },
 })
-let port=process.env.PORT || 5000
-app.listen(port,()=>{
-    console.log("server is running on",port)
+const upload = multer({ storage })
+
+function uploadMiddleware(req, res, next) {
+    try {
+        upload.single('photos')(req, res, function(err) {
+            if(err) res.status(400).json({status: 'error', message: err.message})
+            else next()
+        })
+    } catch (error) {
+        console.log('errorrororororororoor');
+    }
+}
+
+app.post('/file', uploadMiddleware, (req, res) => {
+    console.log('my route')
+    console.log(req.body , req.file)
+    res.end();
+})
+
+app.post('/multipleFile', upload.array('photos', 12), (req, res, next) => {
+    console.log(req.body , req.files)
+    res.end();
+})
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
